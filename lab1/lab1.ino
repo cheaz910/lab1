da6758db372d0535c59dbdeae46f3a0174f1ff62
cheaz910 <cheaz910@mail.ru>
2019-10-12 15:59:13 +0500

    All remaked

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
-#define RGB_COUNT 4
-const byte rgbs[RGB_COUNT][3] = {{22, 23, 24}, 
-                                 {25, 26, 27}, 
-                                 {28, 29, 30}, 
-                                 {31, 32, 33}};
+#define RGB_COUNT 8
+#define DELTA 10;
+
+const byte rgbs[RGB_COUNT] = {22, 23, 24, 25, 26, 27, 28, 29};
+int rgbs_values[RGB_COUNT] = {255, 127, 0, 382, 255, 127, 0, 382};
 
 void setup()
 {
@@ -12,40 +12,26 @@ void setup()
     Serial.println("Started!");
 
     for (int i = 0; i < RGB_COUNT; i++) 
-    {
-        for (int j = 0; j < 3; j++) 
-        {
-            pinMode(rgbs[i][j], OUTPUT);
-        }
-    }
+        pinMode(rgbs[i], OUTPUT);
 }
 
 void loop() 
 {
-    int opposite_rgb = RGB_COUNT / 2;
-    for (int i = 0; i < opposite_rgb; i++)
+    for (int i = 0; i < RGB_COUNT; i++)
     {
-        gradual_turn(i, i + opposite_rgb, true);
-        gradual_turn(i, i + opposite_rgb, false);
+        int possible_value = rgbs_values[i] + DELTA;
+        rgbs_values[i] = possible_value > 500 ? 0 : possible_value;
     }
+    set_rgbs_values();
+    delay(30);
 }
 
-void gradual_turn(int rgb_number1, int rgb_number2, boolean turn_on) {
-    for (int i = 255; i >= 0; i--)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (turn_on)
-            {
-                analogWrite(rgbs[rgb_number1][j], i);
-                analogWrite(rgbs[rgb_number2][j], i);
-            }
-            else
-            {
-                analogWrite(rgbs[rgb_number1][j], 255 - i);
-                analogWrite(rgbs[rgb_number2][j], 255 - i);
-            }
-            delay(10);
-        }
-    }
+void set_rgbs_values() {
+    for (int i = 0; i < RGB_COUNT; i++)
+        analogWrite(rgbs[i], get_true_rgb_value(rgbs_values[i]));
+}
+
+byte get_true_rgb_value(int rgb_value)
+{
+    return rgb_value < 256 ? rgb_value : 510 - rgb_value;
 }
